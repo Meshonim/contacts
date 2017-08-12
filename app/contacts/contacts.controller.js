1d784c1dc07460cceb218a32dd2f7e184f53cb09
Meshonim <ilya.trukhanovich@gmail.com>
2017-08-12 13:38:27 +0300

    Implement filter

--- app/contacts/contacts.controller.js
+++ app/contacts/contacts.controller.js
@@ -6,23 +6,32 @@
         .controller('ContactListController', controller)
 
     controller.$inject = ['$scope', 'Contact'];
-    
+
     function controller($scope, Contact) {
         $scope.contacts = Contact.query();
         $scope.contacts.$promise.then(function(result) {
             $scope.contacts = result;
-       angular.forEach(result, function(contact) {
-    contact.isFavorite = contact.isFavorite === "1";   
-});
-   });
-        //Contact.get({contactid: 1});
-        
-        console.log($scope.contacts);
-
-
-        $scope.changeStatus = function (id, isFavorite)
-        {
-            Contact.favorite({id: id}, {status: isFavorite} );
+            angular.forEach(result, function(contact) {
+                contact.isFavorite = (contact.isFavorite === "1");
+            });
+        });
+        $scope.changeStatus = function(id, isFavorite) {
+            Contact.favorite({
+                id: id
+            }, {
+                status: isFavorite
+            });
         }
+        $scope.criteriaMatch = function(criteria) {
+            return function(contact) {
+                if (criteria === undefined)
+                    return true;
+                if (criteria.length < 2)
+                    return true;
+                var name = contact.first+" "+contact.last;
+                return name.match(new RegExp(criteria, 'i'));
+            }
+        };
+
     }
 })();
